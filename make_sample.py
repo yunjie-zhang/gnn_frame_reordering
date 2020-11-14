07cccb3794e179558a5913caba837326a62c14c1
Yunjie Zhang <admin@admins-MacBook-Pro.local>
2020-11-13 16:03:48 -0800

    Update.

--- make_sample.py
+++ make_sample.py
@@ -30,12 +30,15 @@ def makePicPair(total_cnt, interval):
     print("\t")
     return ret_torch
 
-def load_tsv(tsv_path: str, video_id_set):
+def loadTsv(tsv_path: str, video_name_list_full):
     #video_id -> account
-    video_id2acc = dict()
+    video_name_list_set = set(video_name_list_full)
+    video_from_tsv = set()
+
+    account2video = dict()
     account2info = dict()
     with open(tsv_path, "r") as fp:
-        line = fp.readline()
+        line = fp.readline()#the first line is title
         line = fp.readline()
         while line:
             fields = line.split("\t")
@@ -46,34 +49,81 @@ def load_tsv(tsv_path: str, video_id_set):
             account_str = fields[0]
             video_id_str = fields[1]
             cat_1 = fields[3]
-            if video_id_str not in video_id_set:
+            if video_id_str not in video_name_list_set:
                 line = fp.readline()
                 continue
 
-            if video_id_str not in video_id2acc:
-                video_id2acc[video_id_str] = []
-            video_id2acc[video_id_str].append(account_str)
+            if account_str not in account2video:
+                account2video[account_str] = []
+            account2video[account_str].append(video_id_str)
             account2info[account_str] = cat_1
+            video_from_tsv.add(video_id_str)
             line = fp.readline()
 #    for key in ret_dict.keys():
 #        print("{}\t{}".format(key, len(ret_dict[key])))
 
-    return video_id2acc, account2info
+    video_name_list = []
+    video_candidate_set = video_from_tsv & video_name_list_set
+
+    for video_id in video_candidate_set:
+        video_name_list.append(video_id)
+
+
+    return account2video, account2info, video_name_list
+
+def makeAccPair(account2video, video_name2idx):
+    account2idx = dict()
+    acc_idx = 0
+    for acc_str in account2video.keys():
+        account2idx[acc_str] = acc_idx
+        acc_idx += 1
+
+    account2video_list = []
+    for acc in account2video.keys():
+        for video_id in account2video[acc]:
+            account2video_list.append([acc, video_id])
+
+    account2pic_list = []
+    for acc_video_pair in account2video_list:
+        acc_id = acc_video_pair[0]
+        video_id = acc_video_pair[1]
+        for pic_id in video_name2idx[video_id]:
+            account2pic_list.append([acc_id, pic_id])
+
+    acc_idx2pic_list = []
+    pic2acc_idx_list = []
+    for pair in account2pic_list:
+        acc_idx = account2idx[pair[0]]
+        pic_idx = pair[1]
+    acc_idx2pic_list.append([acc_idx, pic_idx])
+    pic2acc_idx_list.append([pic_idx, acc_idx])
+
+    ret_torch_1 = torch.tensor(acc_idx2pic_list, dtype=torch.int)
+    ret_torch_1 = torch.transpose(ret_torch_1, 0, 1)
+
+    ret_torch_2 = torch.tensor(pic2acc_idx_list, dtype=torch.int)
+    ret_torch_2 = torch.transpose(ret_torch_2, 0, 1)
+
+    return ret_torch_1, ret_torch_2
+
+
+
+
+
 
 def make_graph(tsv_path:str, feature_path: str):
     file_list = os.listdir(feature_path)
-
-
     video_name_list_full = [cur_str.split(".")[0] for cur_str in file_list]
-    video_name_list_set = set(video_name_list_full)
-    video_id2acc, _ = load_tsv(tsv_path, video_name_list_set)
 
 
 
-    video_name_list = []
-    for video_id in video_name_list_full:
-        if video_id in video_id2acc:
-            video_name_list.append(video_id)#to make sure that video_name_list is the intersection of two parts.
+
+
+    account2video, account2info, video_name_list = loadTsv(tsv_path, video_name_list_set)
+
+
+
+
 
 
     print("A total of {} videos found.".format(len(video_name_list)))
@@ -99,8 +149,14 @@ def make_graph(tsv_path:str, feature_path: str):
         if len(video_name2idx[cur_video_id]) != FRAME_CNT:
             print("Frame count doesn't match.")
 
+
+
+    ret_acc2pic_node, ret_pic2acc_node = makeAccPair(account2video, video_name2idx)
     ret_pic_node = makePicPair(video_cnt, FRAME_CNT)
-    g = dgl.heterograph({('pic', 'nb', 'pic'): (ret_pic_node[0], ret_pic_node[1])})
+
+    g = dgl.heterograph({('pic', 'nb', 'pic'): (ret_pic_node[0], ret_pic_node[1]),
+                         ('acc', 'pb', 'pic'): (ret_acc2pic_node[0], ret_acc2pic_node[1]),
+                         ('pic', 'blt', 'pic'): (ret_pic2acc_node[0], ret_pic2acc_node[1])})
     #g = dgl.heterograph({('pic', 'nb', 'pic'):(torch.tensor([0, 0, 2]), torch.tensor([1, 2, 3])), ('acc', 'own', 'pic'):(torch.tensor([0, 1, 0]), torch.tensor([1, 2, 3]))})
     pic_node_num = g.num_nodes('pic')
     print(pic_node_num)
