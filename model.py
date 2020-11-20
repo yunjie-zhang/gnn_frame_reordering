84a17851caa8880a2cd6582c12c31e38beae67c3
Yunjie Zhang <admin@admins-MacBook-Pro.local>
2020-11-20 14:15:19 -0800

    Update model

---
+++ model.py
@@ -0,0 +1,47 @@
+import numpy as np
+import random
+import torch
+import dgl
+import sys
+import os
+from torch import nn
+import torch.nn.functional as F
+import dgl.nn as dglnn
+
+class RGCN(nn.Module):
+    def __init__(self, in_feats, hid_feats, out_feats, rel_names):
+        super().__init__()
+
+        self.conv1 = dglnn.HeteroGraphConv({
+            rel: dglnn.GraphConv(in_feats, hid_feats)
+            for rel in rel_names}, aggregate='sum')
+        self.conv2 = dglnn.HeteroGraphConv({
+            rel: dglnn.GraphConv(hid_feats, out_feats)
+            for rel in rel_names}, aggregate='sum')
+
+    def forward(self, graph, inputs):
+        # inputs are features of nodes
+        h = self.conv1(graph, inputs)
+        h = {k: F.relu(v) for k, v in h.items()}
+        h = self.conv2(graph, h)
+        return h
+
+class HeteroDotProductPredictor(nn.Module):#this can be replaced by a more complicated neural network
+    def forward(self, graph, h, etype):
+        # h contains the node representations for each edge type computed from
+        # the GNN for heterogeneous graphs defined in the node classification
+        # section (Section 5.1).
+        with graph.local_scope():
+            graph.ndata['h'] = h   # assigns 'h' of all node types in one shot
+            graph.apply_edges(fn.u_dot_v('h', 'h', 'score'), etype=etype)
+            return graph.edges[etype].data['score']
+
+class ReOrderingModel(nn.Module):
+    def __init__(self, in_features, hidden_features, out_features, rel_names):
+        super().__init__()
+        self.sage = RGCN(in_features, hidden_features, out_features, rel_names)
+        self.pred = HeteroDotProductPredictor()
+    def forward(self, g, x, etype):
+        h = self.sage(g, x)
+        return self.pred(g, h, etype)
+
