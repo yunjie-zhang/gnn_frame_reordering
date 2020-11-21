211f7e7a0deeb2be0aa67ee3118923673a1ed943
Yunjie Zhang <admin@admins-MacBook-Pro.local>
2020-11-20 17:24:42 -0800

    Update.

--- train.py
+++ train.py
@@ -53,8 +53,9 @@ def train(root_dir: str, meta_data_path: str, batch_size: int):
 
     pic_feats = g.nodes['pic'].data['img_feat']
     acc_feats = g.nodes['acc'].data['acc_feat']
-    print(pic_feats.size)
-    print(acc_feats.size)
+    print(type(pic_feats), type(acc_feats))
+    print(pic_feats.size())
+    print(acc_feats.size())
     exit()
 
 
