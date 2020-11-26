18dc157521650197c669b92f7433b7ac42e841a3
Yunjie Zhang <admin@admins-MacBook-Pro.local>
2020-11-25 16:56:13 -0800

    Update.

--- train.py
+++ train.py
@@ -84,7 +84,7 @@ def train(root_dir: str, meta_data_path: str, batch_size: int):
     num_classes = 1
     #model = GNNRankModel(in_features, hidden_features, out_features, num_classes)
     #model = model.cuda()
-    opt = torch.optim.Adam(model.parameters())
+    #opt = torch.optim.Adam(model.parameters())
     for epoch in range(epoch_cnt):
         print("Epoch {}".format(epoch))
         for input_nodes, edge_subgraph, blocks in dataloader:
