7f7eac8153e34add0c37199addad2d23cb13b1a0
Yunjie Zhang <admin@admins-MacBook-Pro.local>
2020-11-26 00:43:03 -0800

    Update.

--- train.py
+++ train.py
@@ -109,7 +109,7 @@ def train(root_dir: str, meta_data_path: str, batch_size: int):
             print(type(edge_labels), type(edge_predictions))
             #edge_predictions = model(edge_subgraph, blocks, input_features)
             #loss = compute_loss(edge_labels, edge_predictions)
-            
+
             loss = loss_fn(edge_predictions[('pic', 'nb', 'pic')], edge_labels[('pic', 'nb', 'pic')])
             opt.zero_grad()
             loss.backward()
