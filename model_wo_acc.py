e21ea0a19830a1880a0e47a05e001e4115555b0a
Jimmyzzz <39707082+yunjie-zhang@users.noreply.github.com>
2021-04-13 14:41:07 -0700

    Update model_wo_acc.py

--- model_wo_acc.py
+++ model_wo_acc.py
@@ -8,95 +8,14 @@ from torch import nn
 import torch.nn.functional as F
 import dgl.nn as dglnn
 import dgl.function as fn
-"""
-class MLPPredictor(nn.Module):
-    def __init__(self, in_features, out_classes):
-        super().__init__()
-        self.W = nn.Linear(in_features * 2, out_classes)
-
-    def apply_edges(self, edges):
-        h_u = edges.src['h']
-        h_v = edges.dst['h']
-        score = self.W(torch.cat([h_u, h_v], 1))
-        return {'score': score}
-
-    def forward(self, graph, h, etype):
-        # h contains the node representations for each edge type computed from
-        # the GNN for heterogeneous graphs defined in the node classification
-        # section (Section 5.1).
-        with graph.local_scope():
-            graph.ndata['h'] = h   # assigns 'h' of all node types in one shot
-            graph.apply_edges(self.apply_edges, etype=etype)
-            return graph.edges[etype].data['score']
-
-class RGCN(nn.Module):
-    def __init__(self, in_feats, hid_feats, out_feats, rel_names):
-        super().__init__()
-
-        self.conv1 = dglnn.HeteroGraphConv({
-            rel: dglnn.GraphConv(in_feats, hid_feats)
-            for rel in rel_names}, aggregate='sum')
-        self.conv2 = dglnn.HeteroGraphConv({
-            rel: dglnn.GraphConv(hid_feats, out_feats)
-            for rel in rel_names}, aggregate='sum')
-
-    def forward(self, graph, inputs):
-        # inputs are features of nodes
-        h = self.conv1(graph, inputs)
-        h = {k: F.relu(v) for k, v in h.items()}
-        h = self.conv2(graph, h)
-        return h
-
-class HeteroDotProductPredictor(nn.Module):#this can be replaced by a more complicated neural network
-    def forward(self, graph, h, etype):
-        # h contains the node representations for each edge type computed from
-        # the GNN for heterogeneous graphs defined in the node classification
-        # section (Section 5.1).
-        with graph.local_scope():
-            graph.ndata['h'] = h   # assigns 'h' of all node types in one shot
-            graph.apply_edges(fn.u_dot_v('h', 'h', 'score'), etype=etype)
-            return graph.edges[etype].data['score']
-
-class ReOrderingModel(nn.Module):
-    def __init__(self, in_features, hidden_features, out_features, rel_names):
-        super().__init__()
-        self.sage = RGCN(in_features, hidden_features, out_features, rel_names)
-        self.pred = HeteroDotProductPredictor()
-    def forward(self, g, x, etype):
-        h = self.sage(g, x)
-        return self.pred(g, h, etype)
-"""
-class RGCN(nn.Module):
-    def __init__(self, in_feats, hid_feats, out_feats, rel_names):
-        super().__init__()
-
-        self.conv1 = dglnn.HeteroGraphConv({
-            rel: dglnn.GraphConv(in_feats, hid_feats)
-            for rel in rel_names}, aggregate='sum')
-        self.conv2 = dglnn.HeteroGraphConv({
-            rel: dglnn.GraphConv(hid_feats, out_feats)
-            for rel in rel_names}, aggregate='sum')
-
-    def forward(self, graph, inputs):
-        # inputs are features of nodes
-        h = self.conv1(graph, inputs)
-        h = {k: F.relu(v) for k, v in h.items()}
-        h = self.conv2(graph, h)
-        return h
 
 class StochasticTwoLayerRGCN(nn.Module):
     def __init__(self, in_feat, hidden_feat, out_feat, rel_names):
         super().__init__()
-        self.conv1 = dglnn.HeteroGraphConv({
-                rel : dglnn.GraphConv(in_feat, hidden_feat, norm='both',
+        self.conv1 = dglnn.GraphConv(in_feat, hidden_feat, norm='both',
                                       weight=True, bias=True, activation=nn.ReLU())
-                for rel in rel_names
-            })
-        self.conv2 = dglnn.HeteroGraphConv({
-                rel : dglnn.GraphConv(hidden_feat, out_feat, norm='both',
+        self.conv2 = dglnn.GraphConv(hidden_feat, out_feat, norm='both',
                                       weight=True, bias=True, activation=nn.ReLU())
-                for rel in rel_names
-            })
 
     def forward(self, blocks, x):
         x = self.conv1(blocks[0], x)
